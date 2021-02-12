6601745cc5f5c377a6f6785def0499103b0effb7
micro-chipset <tyrnov.ivan@gmail.com>
2021-02-12 21:14:13 +0300

    change input to Field

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
-import {Formik} from 'formik';
+import {
+    Formik,
+    Field,
+    Form,
+} from 'formik';
 import classNames from 'classnames/bind';
 
 import * as styles from './login.scss';
@@ -16,42 +20,25 @@ export const Login = (): JSX.Element => (
                 }, 400);
             }}
         >
-            {({
-                values,
-                handleChange,
-                handleBlur,
-                handleSubmit
-              }) => (
-                  <form
-                      onSubmit={handleSubmit}
-                      className={cn('form')}
-                  >
-                      <input
-                        type='text'
-                        name='login'
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.login}
+            {() => (
+                <Form className={cn('form')}>
+                    <Field
+                        type="text"
+                        name="login"
                         className={cn('form__login')}
-                      />
-
-                      <input
-                        type='password'
-                        name='password'
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.password}
+                    />
+                    <Field
+                        type="password"
+                        name="password"
                         className={cn('form__password')}
-                      />
-                      <button
-                        type='submit'
-                        className={cn('form__button')}
-                      >
-                          Войти
-                      </button>
-                  </form>
+                    />
+                    <button
+                        type="submit"
+                        className={cn('form__button')}>
+                        Submit
+                    </button>
+                </Form>
             )}
         </Formik>
     </div>
-
 )
